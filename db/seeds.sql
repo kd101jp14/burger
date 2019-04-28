335361d9396efeba049642a9b225c68e1ab3741c
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-28 13:22:45 -0500

    Created seeds for the 'burgers' table.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers ("burger_name", "devoured") VALUES ("#1 Burger", true);
+INSERT INTO burgers ("burger_name", "devoured") VALUES ("Burger Burger", false);
+INSERT INTO burgers ("burger_name", "devoured") VALUES ("Good Burger", true);
