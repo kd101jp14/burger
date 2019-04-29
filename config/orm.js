ce49b7eea51428c8cdb7fabf5e0a6df0299499ed
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-29 12:23:42 -0500

    Attempt at 'insertOne' method.

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,27 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function(callback) {
-        var queryString = "SELECT * FROM burgers_db";
-    
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-              }
-              callback(result);
-        });
-    }
-  };
-  
-  module.exports = orm;
-  
+  selectAll: function(callback) {
+    var queryString = "SELECT * FROM burgers_db";
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      callback(result);
+    });
+  },
+  insertOne: function(req, res, callback) {
+    var queryString = "INSERT INTO burgers_db SET ?";
+    var values = { burger_name: req.body.burger_name, devoured: false };
+
+    connection.query(queryString, values, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      callback(result);
+    });
+  }
+};
+
+module.exports = orm;
