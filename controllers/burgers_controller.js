e405be060ff98dccfacaaabc8afc5a4a3e4a3595
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-30 10:59:38 -0500

    Added most of the neccessary code for the index view.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,19 +12,19 @@ module.exports = function(app) {
     });
   });
 
-  app.post("/", function(req, res) {
-    burger.insertOne(
-      ["burger_name", "devoured"],
-      [req.body.burger_name, req.body.devoured],
-      function(results) {
-        res.render("index", results);
-      }
-    );
-  });
+  // app.post("/", function(req, res) {
+  //   burger.insertOne(
+  //     ["burger_name", "devoured"],
+  //     [req.body.burger_name, req.body.devoured],
+  //     function(results) {
+  //       res.render("index", results);
+  //     }
+  //   );
+  // });
 
-  app.put("/", function(req, res) {
-    burger.updateOne("devoured", req.body.devoured, function(results) {
-      res.render("index", results);
-    });
-  });
+  // app.put("/", function(req, res) {
+  //   burger.updateOne("devoured", req.body.devoured, function(results) {
+  //     res.render("index", results);
+  //   });
+  // });
 };
