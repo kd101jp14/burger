87df41afd8676e12bcb9f089d22648737f29a32c
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-29 23:19:13 -0500

    Updated controller.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,28 +4,27 @@ var app = express();
 
 var burger = require("../models/burger.js");
 
-app.get("/", function(req, res) {
-  orm.selectAll(function(results) {
-    callback(results);
-    res.render("index", results);
-  });
-});
-
-app.post("/", function(req, res) {
-  burger.insertOne(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
-    function(results) {
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    orm.selectAll(function(results) {
+      callback(results);
       res.render("index", results);
-    }
-  );
-});
-
-app.put("/", function(req, res) {
-  burger.updateOne("devoured", req.body.devoured, function(results) {
-    res.render("index", results);
+    });
   });
-});
 
-// Export routes for server.js to use.
-module.exports = app;
+  app.post("/", function(req, res) {
+    burger.insertOne(
+      ["burger_name", "devoured"],
+      [req.body.burger_name, req.body.devoured],
+      function(results) {
+        res.render("index", results);
+      }
+    );
+  });
+
+  app.put("/", function(req, res) {
+    burger.updateOne("devoured", req.body.devoured, function(results) {
+      res.render("index", results);
+    });
+  });
+};
