becd45b5c2fb1a5e639685cd9c598820001378f1
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-30 16:47:46 -0500

    Added 'NOT NULL' to 'devoured' column of database.

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE burgers
 (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
