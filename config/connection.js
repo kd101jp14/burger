8864cf4b686bd32ac1f85f3075390a227e7136fa
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-30 16:21:20 -0500

    Updated connection so that app can use JawsDB.

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,20 @@ var mysql = require("mysql");
 // Require sercret password
 var passwordModule = require("../password_module.js");
 
-// Set up MySQL connection
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: passwordModule.password,
-  database: "burgers_db"
-});
+var connection;
+
+// Hook up app with JawsDB or set up MySQL connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: passwordModule.password,
+    database: "burgers_db"
+  });
+};
 
 // Make connection
 connection.connect(function(err) {
