c8c81140a02fea8fb8e7d99d7505d5ab8eddd01c
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-05-03 20:11:56 -0500

    Cleaned up code/Removed unnecessary code. Added app screenshot.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -45,35 +45,3 @@ router.put("/api/burgers/:id", function(req, res) {
 
 // Export routes for server.js to use.
 module.exports = router;
-
-
-// module.exports = function(app) {
-//   app.get("/", function(req, res) {
-//     orm.selectAll(function(result) {
-//       res.render("index", result);
-//     });
-//   });
-
-//   app.post("/", function(req, res) {
-//     burger.insertOne(
-//       ["burger_name", "devoured"],
-//       [req.body.burger_name, req.body.devoured],
-//       function(result) {
-//         res.json(result);
-//       }
-//     );
-//   });
-
-//   app.put("/", function(req, res) {
-//     burger.updateOne({
-//       devoured: req.body.devoured
-//     },
-//     function(result) {
-//       if (result.changedRows === 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       }
-//       res.status(200).end();
-//     });
-//   });
-// };
